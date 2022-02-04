6162523ad55a49178c46660344d888d99f5050a5
dimon08888 <dimon08888@mail.ru>
2022-02-04 13:52:12 +0300

    Создан проект на TS сделан запрос на API
    отрисовка данных в браузер

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+
+type Product = {
+  id: number;
+  image: string;
+  price: number;
+  title: string;
+  category: string;
+  description: string;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Header />
+      <Products />
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <input type="text" />
+      <button></button>
+    </header>
+  );
+}
+
+function ProductCard({ product }: { product: Product }) {
+  return (
+    <div>
+      {product.title}
+      <img width={100} height={100} src={product.image} alt="" />
+    </div>
+  );
+}
+
+function Products() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('http://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(setProducts);
+  }, []);
+
+  return (
+    <div>
+      {products.map(product => (
+        <ProductCard key={product.id} product={product} />
+      ))}
     </div>
   );
 }
